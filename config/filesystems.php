c815f222443bcd89831f968bc3197951d5ba58b8
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-23 10:14:19 -0700

    changed filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,8 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            //'root' => storage_path('app/public'),
-            'root' => public_path(),
+            'root' => storage_path('app/public'),
+//            'root' => public_path(),
             'visibility' => 'public',
         ],
 
