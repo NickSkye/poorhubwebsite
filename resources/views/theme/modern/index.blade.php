a49123778a8fb68521557d0f343329a40ff8b110
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-26 12:21:07 -0700

    added state

--- resources/views/theme/modern/index.blade.php
+++ resources/views/theme/modern/index.blade.php
@@ -86,7 +86,9 @@
                                     <select class="form-control select2" id="state_select" name="state">
                                         <option value=""> @lang('app.select_state') </option>
 
-                                       
+                                        @foreach($country_usage->states as $state)
+                                            <option value="{{ $state->id }}" {{ request('state') ==  $state->id ? 'selected':'' }} >{{ $state->state_name }}</option>
+                                        @endforeach
 
                                     </select>
                                 </div>
